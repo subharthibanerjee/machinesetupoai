7e6c8848394821a134dd9de8a4c09268b8b67dc2
telenb <sbanerjee5@unl.edu>
2019-07-12 17:37:23 -0500

    commit not tested

--- installsys.sh
+++ installsys.sh
@@ -19,49 +19,100 @@ function echo_debug() {
 }
 echo_debug "`date`: start installing USRP and libboost"
 
-sudo apt-get -y install libboost-all-dev
-sudo add-apt-repository ppa:ettusresearch/uhd
-sudo apt-get update
-sudo apt-get -y install libuhd-dev libuhd003 uhd-host
+
+if  [[ `lsb_release -rs` == "18.04" ]]; then
+	echo_info "Installing USRP"
+
+	sudo apt-get -y install libboost-all-dev libusb-1.0-0-dev python-mako doxygen python-docutils python-requests cmake build-essential
+	git clone git://github.com/EttusResearch/uhd.git
+	cd uhd; mkdir host/build; cd host/build
+	cmake -DCMAKE_INSTALL_PREFIX=/usr ..
+	make -j4
+	sudo make install
+	sudo ldconfig
+	sudo /usr/lib/uhd/utils/uhd_images_downloader.py
+	echo_success "`date`: Successfully installed USRP "
+
+	
+
+	echo_info "Installing patches"
+	cd ~/Downloads
+	wget open-cells.com/d5138782a8739209ec5760865b1e53b0/opencells-mods-20190621.tgz
+	
+	tar xf ~/Downloads/opencells-mods-20190621.tgz
+	
+	echo_succes "`date`: Installed patches"
+	echo_info "Installing and checking out develop branch of openairinterface5g" 
+
+	git clone https://gitlab.eurecom.fr/oai/openairinterface5g.git
+	cd openairinterface5g
+	git checkout develop
+	git checkout "17b9a9e917ce2a3a8c7004c7b9a221c350ddfe17"
+	if [ $? -eq 0 ]; then
+		echo_success "`date` checkout successful"
+	else
+		echo_debug "`date` failed checkout"
+		exit
+	fi
+
+	cp ~/Downloads/opencells-mods/cmake_targets/tools/build_helper ~/openairnterface5g/cmake_targets/tools
 
 
-echo_success "installation complete for USRP and libboost"
+	echo_info "copy complete"
 
-mkdir -p ~/openairinterface5g
+	source oaienv
+	echo_debug $PWD
+	./cmake_targets/build_oai -I       # install SW packages from internet
+	./cmake_targets/build_oai  -w USRP --eNB --UE --noS1 -x -c -C# compile eNB
+else
+	sudo apt-get -y install libboost-all-dev
+	sudo add-apt-repository ppa:ettusresearch/uhd
+	sudo apt-get update
+	sudo apt-get -y install libuhd-dev libuhd003 uhd-host
 
-OPENAIRHOME=~/openairinterface5g
-cd $HOME
-echo_info "`date`:git cloning https://gitlab.eurecom.fr/oai/openairinterface5g"
-git clone https://gitlab.eurecom.fr/oai/openairinterface5g
+	echo_success "installation complete for USRP and libboost"
 
-echo_success "cloning complete"
+	mkdir -p ~/openairinterface5g
 
-cd $OPENAIRHOME/cmake_targets/
-echo_info "current directory $PWD"
-echo_info "`date`: Installing dependencies"
+	OPENAIRHOME=~/openairinterface5g
+	cd $HOME
+	echo_info "`date`:git cloning https://gitlab.eurecom.fr/oai/openairinterface5g"
+	git clone https://gitlab.eurecom.fr/oai/openairinterface5g
 
-./build_oai -I
+	echo_success "cloning complete"
 
-echo_success "`date`:Installation complete"
+	cd $OPENAIRHOME/cmake_targets/
+	echo_info "current directory $PWD"
+	echo_info "`date`: Installing dependencies"
 
-./build_oai --UE --noS1 -c -w USRP -x
+	./build_oai -I
 
-cd $OPENAIRHOME
-source oaienv
-echo_info "`date`:sourced OAIENV"
+	echo_success "`date`:Installation complete"
 
-source ./targets/bin/init_nas_nos1 UE
+	./build_oai --UE --noS1 -c -w USRP -x
 
-echo_success "`date`:nashmesh complete"
+	cd $OPENAIRHOME
+	source oaienv
+	echo_info "`date`:sourced OAIENV"
+
+	source ./targets/bin/init_nas_nos1 UE
+
+	echo_success "`date`:nashmesh complete"
 
 
 
-cd $OPENAIRHOME/cmake_targets
+	cd $OPENAIRHOME/cmake_targets
 
-sudo /usr/lib/uhd/utils/uhd_images_downloader.py
+	sudo /usr/lib/uhd/utils/uhd_images_downloader.py
 
-echo_info "downloaded installer"
-<<<<<<< HEAD
+	echo_info "downloaded installer"
+fi
+
+
+
+
+
+
 
 
 if 
@@ -70,5 +121,5 @@ sudo -E ./lte_noS1_build_oai/build/lte-uesoftmodem-nos1 -C $dl_frequency -r 25 \
  --phy-test
 =======
 sudo -E ./cmake_targets/lte_noS1_build_oai/build/lte-uesoftmodem-nos1 -d --phy-test -U 1 -C 750000000 -r 25 --ue-scan-carrier --ue-txgain 90 --ue-rxgain 115 --ue-max-power -5
->>>>>>> 13814358f8e706262bc1607b25cc4c3aff3dc240
+
 
--- kernel.sh
+++ kernel.sh
@@ -18,28 +18,42 @@ function echo_debug() {
 
 echo_info "Updating the system"
 
-apt-get -y update
+sudo apt-get -y update
 
 echo_info "Collecting general information regarding the system"
-if  [[ `lsb_release -rs` == "18.04" ]]
+
+echo_info "Following information from https://open-cells.com/index.php/2017/08/22/all-in-one-openairinterface-august-22nd/"
+# https://open-cells.com/index.php/2017/08/22/all-in-one-openairinterface-august-22nd/
+
+
+if  [[ `lsb_release -rs` == "18.04" ]]; then
 	echo_info "Installing low latency kernel for 18.04"
+	sudo apt-get -y install subversion git i7z
 
+	echo -n | openssl s_client -showcerts -connect gitlab.eurecom.fr:443 2>/dev/null | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' | sudo tee -a /etc/ssl/certs/ca-certificates.crt
+
+	
+	
+else
+	echo_info "Installing low latency kernel for 14.04"
+	apt-get -y install linux-image-3.19.0-61-lowlatency linux-headers-3.19.0-61-lowlatency
+	apt-get -y install subversion git i7z cpufrequtils
+	#add power options to grub, eliminating c and p states and the rest
+	sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="quiet splash"/GRUB_CMDLINE_LINUX_DEFAULT="quiet splash intel_pstate=disable processor.max_cstate=1 intel_idle.max_cstate=0 idle=poll"/g' /etc/default/grub
+	update-grub
+	#check power settings in cpufrequtils, adding line if necessary
+	grep -q -F 'GOVERNOR="performance"' /etc/default/cpufrequtils
+	if [ $? -ne 0 ]; then
+	  echo 'GOVERNOR="performance"' >> /etc/default/cpufrequtils
+	fi
+	#update and restart
+	update-rc.d ondemand disable
+	/etc/init.d/cpufrequtils restart
+	#run i7z to check if all cores are running at 100%, should be done after a restart of the cpu because the new kernel must be chosen from the "advanced options" page in the grub
+	echo_debug "Please restart the computer and then choose advanced options > 3.19 kernel from the grub screen. Run sudo i7z to check if the power options are correct, cores must be running at 100%. "
 fi
 
-apt-get -y install linux-image-3.19.0-61-lowlatency linux-headers-3.19.0-61-lowlatency
+
 #necessary programs
-apt-get -y install subversion git i7z cpufrequtils
-#add power options to grub, eliminating c and p states and the rest
-sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="quiet splash"/GRUB_CMDLINE_LINUX_DEFAULT="quiet splash intel_pstate=disable processor.max_cstate=1 intel_idle.max_cstate=0 idle=poll"/g' /etc/default/grub
-update-grub
-#check power settings in cpufrequtils, adding line if necessary
-grep -q -F 'GOVERNOR="performance"' /etc/default/cpufrequtils
-if [ $? -ne 0 ]; then
-  echo 'GOVERNOR="performance"' >> /etc/default/cpufrequtils
-fi
-#update and restart
-update-rc.d ondemand disable
-/etc/init.d/cpufrequtils restart
-#run i7z to check if all cores are running at 100%, should be done after a restart of the cpu because the new kernel must be chosen from the "advanced options" page in the grub
-echo "Please restart the computer and then choose advanced options > 3.19 kernel from the grub screen. Run sudo i7z to check if the power options are correct, cores must be running at 100%. "
+
 
