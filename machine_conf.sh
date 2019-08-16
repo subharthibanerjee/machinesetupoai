17cf8968faf0afea26f40a69111c441a59f342e1
subharthi <sbaneree5@unl.edu>
2019-08-15 19:34:41 -0500

    add cset

--- machine_conf.sh
+++ machine_conf.sh
@@ -1,5 +1,23 @@
 #! /bin/sh
+Red=$'\e[1;31m'
+Green=$'\e[1;32m'
+Blue=$'\e[1;34m'
+Default=$'\e[0'
 
+function echo_info() {
+	echo -e "$Blue \b"$1
+}
+function echo_sucess() {
+	echo -e "$Green \b"$1
+}
+
+function echo_debug() {
+	echo -e "$Red \b"$1
+}
+function echo_default() {
+	echo -e "$Default \b"$1
+}
+sudo bash
 echo 'setting real-time performance measure'
 for cpu in /sys/devices/system/cpu/cpu[0-9]* ; do
 	echo -n "siblings for virtual core:  ${cpu##*/} "
@@ -14,3 +32,18 @@ for f in /proc/irq/[0-9]* ; do echo '0' > $f/smp_affinity_list; done
 
 echo 'Move all the IRQ signals to the same core as the I/Q manager'
 echo '3' > /proc/irq/30/smp_affinity_list
+
+echo "======================================"
+echo_info "Please read carefully from here"
+read -p "Do you want to CSET process? " -n 1 -r
+if [[ ! $REPLY =~ ^[[Yy]$ ]]
+then
+	echo_debug "processing CSET request"
+	echo_default "..."	
+	cset shield --force --kthread on -c 1-3
+	for f in /sys/device/system/cpu/cpu[0-9]* ; do
+		echo "Performance" > $f/cpufreq/scaling_governor
+	done
+fi 
+
+echo "exiting shell ......."
